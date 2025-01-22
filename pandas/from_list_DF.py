505aa0ffa5116602d4ea8d869cbc27cdf453b64d
Maninsalsa <157923258+Maninsalsa@users.noreply.github.com>
2025-01-22 01:25:43 -0800

    pandas

---
+++ pandas/from_list_DF.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+student_data = [[1,15],[2,11],[3,11],[4,20]]
+
+# 2877. Create a DataFrame from List
+# WRite a solution to create a DataFrame from a 2D list called student_data.
+# This 2D list contains the IDs and ages of some students
+
+def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
+    df = pd.DataFrame(student_data, columns=['student_id', 'age'])
+    return df
+
+test = createDataframe(student_data)  # Fixed capitalization of function name
+print(test)
