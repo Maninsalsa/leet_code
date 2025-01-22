58d9f943ccc8531d6c5ea4b4bc477e13b492345b
Maninsalsa <157923258+Maninsalsa@users.noreply.github.com>
2025-01-22 01:27:46 -0800

    Update from_list_DF.py

--- pandas/from_list_DF.py
+++ pandas/from_list_DF.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+# 2877. Create a DataFrame from List
+
 student_data = [[1,15],[2,11],[3,11],[4,20]]
 
-# 2877. Create a DataFrame from List
-# WRite a solution to create a DataFrame from a 2D list called student_data.
-# This 2D list contains the IDs and ages of some students
+"""
+Write a solution to create a DataFrame from a 2D list called student_data.
+This 2D list contains the IDs and ages of some students
+"""
 
 def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
     df = pd.DataFrame(student_data, columns=['student_id', 'age'])
